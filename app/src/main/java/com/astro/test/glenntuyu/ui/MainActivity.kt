54e9af1cfe2d95fdc0707e8961375339c321633d
glenntuyu <glenn@happy5.co>
2022-05-26 09:43:32 +0700

    initial commit project

---
+++ app/src/main/java/com/astro/test/glenntuyu/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.astro.test.glenntuyu.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.astro.test.glenntuyu.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
